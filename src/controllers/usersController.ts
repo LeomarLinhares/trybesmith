506ac79c76d29bcc9714d12d8b7d8bb2d402a31d
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-23 16:19:20 -0300

    :sparkles: cria o usersController com createUser

---
+++ src/controllers/usersController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+import usersService from '../services/usersService';
+
+export const createUser = async (req: Request, res: Response): Promise<void> => {
+  const response = await usersService.create(req.body);
+  res.status(201).json(response);
+};
