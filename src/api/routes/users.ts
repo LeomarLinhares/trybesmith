8baa02a4817529c7e2936a09817ae5a62124a79a
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-23 16:29:43 -0300

    :sparkles: cria a rota users

---
+++ src/api/routes/users.ts
@@ -0,0 +1,39 @@
+import 'express-async-errors';
+import { Router } from 'express';
+import { createUser } from '../../controllers/usersController';
+
+import {
+  validateClasseLength,
+  validateIfClasseExist,
+  validateIfClasseIsAString,
+  validateIfLevelExist,
+  validateIfLevelIsANumber,
+  validateIfLevelIsNotZero,
+  validateIfNameExist,
+  validateIfNameIsAString,
+  validateIfPasswordExist,
+  validateIfPasswordIsAString,
+  validateNameLength,
+  validatePasswordLength,
+} from '../../middlewares';
+
+const users = Router();
+
+users.post(
+  '/users', 
+  validateIfNameExist,
+  validateIfNameIsAString,
+  validateNameLength,
+  validateIfClasseExist,
+  validateIfClasseIsAString,
+  validateClasseLength,
+  validateIfLevelExist,
+  validateIfLevelIsANumber,
+  validateIfLevelIsNotZero,
+  validateIfPasswordExist,
+  validateIfPasswordIsAString,
+  validatePasswordLength,
+  createUser,
+);
+
+export default users;
