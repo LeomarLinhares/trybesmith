6e420dfad2dce3360f85c405617a247c0c406ea5
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-23 16:22:58 -0300

    :sparkles: cria todas as mensagens de erro

---
+++ src/middlewares/error/index.ts
@@ -0,0 +1,15 @@
+export default {
+  ER_USERNAME_REQUIRED: 'Username is required',
+  ER_USERNAME_NOT_A_STRING: 'Username must be a string',
+  ER_USERNAME_LENGTH: 'Username must be longer than 2 characters',
+  ER_CLASSE_REQUIRED: 'Classe is required',
+  ER_CLASSE_NOT_A_STRING: 'Classe must be a string',
+  ER_CLASSE_LENGTH: 'Classe must be longer than 2 characters',
+  ER_PASSWORD_REQUIRED: 'Password is required',
+  ER_PASSWORD_NOT_A_STRING: 'Password must be a string',
+  ER_PASSWORD_LENGTH: 'Password must be longer than 7 characters',
+  ER_LEVEL_REQUIRED: 'Level is required',
+  ER_LEVEL_NOT_A_NUMBER: 'Level must be a number',
+  ER_LEVEL_CANNOT_BE_ZERO_OR_LESS: 'Level must be greater than 0',
+
+};
