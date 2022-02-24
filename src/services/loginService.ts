036f2cd7ae9a9bb6f3b89f0b4a7130cb559e1eba
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-24 14:00:37 -0300

    :sparkles: cria o módulo loginService com a service login

---
+++ src/services/loginService.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import JWT from 'jsonwebtoken';
+import { IErrorMessage } from '../interfaces/IErrorMessage';
+import { IPayload } from '../interfaces/IPayload';
+import { IToken } from '../interfaces/IToken';
+import userModel from '../models/userModel';
+
+dotenv.config();
+
+const secret: string = process.env.JWT_SECRET || 'segredoSecretoDoServer';
+
+export default {
+  login: async (
+    usernameReq: string,
+    passwordReq: string,
+  ): Promise<IToken | IErrorMessage | void> => {
+    try {
+      const [{ id, username }]: IPayload[] = await userModel
+        .getByUsernameAndPassword(usernameReq, passwordReq);
+      const token = JWT.sign({ id, username }, secret, { algorithm: 'HS256' });
+      return { token };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
