3a15343a4b926c18486cb0b7315a2f90df91da9c
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-24 13:58:12 -0300

    :construction: cria a querie GET_BY_USERNAME_AND_PASS

--- src/models/queries/index.ts
+++ src/models/queries/index.ts
@@ -1,4 +1,6 @@
 export default {
   CREATE_USER: `INSERT INTO Trybesmith.Users (username, classe, level, password)
     VALUES (?, ?, ?, ?)`,
+  GET_BY_USERNAME_AND_PASS: `SELECT id, username FROM Trybesmith.Users
+    WHERE username = ? AND password = ?`,
 };
