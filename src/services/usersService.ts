7013c91d5d0a95d747eb822b577451309fb7d1af
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-25 17:55:14 -0300

    :recycle: refatora a geração de token

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import JWT, { SignOptions } from 'jsonwebtoken';
+import JWT from 'jsonwebtoken';
 import { IToken } from '../interfaces/IToken';
 import { IUser } from '../interfaces/IUser';
 import userModel from '../models/userModel';
@@ -8,17 +8,12 @@ dotenv.config();
 
 const secret: string = process.env.JWT_SECRET || 'segredoSecretoDoServer';
 
-const JWTConfig: SignOptions = {
-  expiresIn: '7d',
-  algorithm: 'HS256',
-};
-
 export default {
   create: async (body: IUser): Promise<IToken | void> => {
     try {
       const { insertId: id } = await userModel.create(body);
       const { username } = body;
-      const token = JWT.sign({ id, username }, secret, JWTConfig);
+      const token = JWT.sign({ id, username }, secret, { algorithm: 'HS256' });
       return { token };
     } catch (error) {
       console.log(error);
