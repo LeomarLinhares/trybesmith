d7e73247c83d3f5708913266d4a4b854e36e50fb
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-25 18:00:17 -0300

    :construction: adiciona as rotas ao app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import loginRoute from './api/routes/login';
+import productsRoute from './api/routes/products';
 import usersRoute from './api/routes/users';
 
 const app = express();
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use(usersRoute);
 app.use(loginRoute);
+app.use(productsRoute);
 
 export default app;
