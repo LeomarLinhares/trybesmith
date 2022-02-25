2b7ff27dc5ac2a8d9cf9f52c03a4b133ed67cb6e
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-25 18:31:28 -0300

    :construction: cria a querie GET_ALL_PRODUCTS

--- src/models/queries/index.ts
+++ src/models/queries/index.ts
@@ -4,4 +4,5 @@ export default {
   GET_BY_USERNAME_AND_PASS: `SELECT id, username FROM Trybesmith.Users
     WHERE username = ? AND password = ?`,
   CREATE_PRODUCT: 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+  GET_ALL_PRODUCTS: 'SELECT * FROM Trybesmith.Products',
 };
