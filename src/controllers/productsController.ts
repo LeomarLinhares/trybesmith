f39a19016cd00b35a18a007ef1959f360d54bc52
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-25 18:29:12 -0300

    :sparkles: cria o controller getProducts em productsController

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
 import productsService from '../services/productsService';
 
-const createProduct = async (req: Request, res: Response): Promise<void> => {
+export const createProduct = async (req: Request, res: Response): Promise<void> => {
   const { name, amount } = req.body;
   const response = await productsService.create({ name, amount });
   res.status(201).json(response);
 };
 
-export default createProduct;
+export const getProducts = async (_req: Request, res: Response): Promise<void> => {
+  const response = await productsService.getAll();
+  res.status(200).json(response);
+};
